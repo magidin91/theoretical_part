143c17d0585802d63ba4ddc2889b3bc93d8ebf5a
magidin91 <magidin@mail.ru>
2020-09-13 02:05:26 +0500

    2. common

---
+++ src/main/java/common/Alfa.java
@@ -0,0 +1,23 @@
+package common;
+
+class Alfa {
+ Alfa getThis() {
+ return this;
+ }
+ 
+ public void doAlfa() {
+ System.out.println("что-то делаем в Alfa");
+ }
+}
+
+class Beta extends Alfa {
+ public void doBeta() {
+ System.out.println("что-то делаем в Beta");
+ }
+}
+
+class Omega extends Alfa{
+ public void doOmega() {
+  System.out.println("что-то делаем с Omega");
+ }
+}
