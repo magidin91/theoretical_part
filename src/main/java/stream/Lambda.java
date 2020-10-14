5add182a06977412dd72e407f975957bb50b8bc2
magidin91 <magidin@mail.ru>
2020-10-14 21:40:47 +0500

    5. Stream

--- src/main/java/stream/Lambda.java
+++ src/main/java/stream/Lambda.java
@@ -3,8 +3,13 @@ package stream;
 import java.util.function.Consumer;
 
 public class Lambda {
+    public void print(String string) {
+        System.out.println(string);
+    }
+
     public static void main(String[] args) {
-        Consumer<String> printer = System.out::println; //создали лямбду
+        // Consumer<String> printer = System.out::println; //создали лямбду
+        Consumer<String> printer = string -> new Lambda().print(string); //создали лямбду c использованием метода класса
         printer.accept("Hello"); // вызвали лямбду через функц. интерфейса, передав в его метод аргумент
     }
 }
---
+++ src/main/java/stream/ZAhavatPeremennoi.java
@@ -0,0 +1,10 @@
+package stream;
+
+public class ZAhavatPeremennoi {
+    public static void main(String[] args) {
+//        int s = 10;
+//        Runnable r = () -> { System.out.println(s); };
+//        s = 20;
+//        r.run();
+    }
+}
